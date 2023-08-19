40cf9c339122099590c731b9b6734df24054b464
Christine <christine@zipcodersMBP13.localdomain>
2023-08-19 12:58:33 -0400

    accidentally copied over a bit too much LOLG

--- src/main/java/io/zipcoder/persistenceapp/controllers/DepartmentController.java
+++ src/main/java/io/zipcoder/persistenceapp/controllers/DepartmentController.java
@@ -39,40 +39,4 @@ public class DepartmentController {
     public Boolean delete(@PathVariable Long id){
         return service.delete(id);
     }
-
-    @RestController
-    public static class EmployeeController {
-        @Autowired
-        private io.zipcoder.persistenceapp.services.EmployeeService service;
-
-        public EmployeeController(io.zipcoder.persistenceapp.services.EmployeeService service) {
-            this.service = service;
-        }
-
-        @PostMapping("/employees")
-        public io.zipcoder.persistenceapp.models.Employee create(@RequestBody io.zipcoder.persistenceapp.models.Employee employee){
-            return service.create(employee);
-        }
-
-        @GetMapping("/employees")
-        public Iterable<io.zipcoder.persistenceapp.models.Employee> getAll(){
-            return service.findAll();
-        }
-
-        @GetMapping("/employees/{id}")
-        public io.zipcoder.persistenceapp.models.Employee getOne(@PathVariable Long id){
-            return service.findById(id);
-        }
-
-        @PutMapping("/employees/{id}")
-        public io.zipcoder.persistenceapp.models.Employee update(@PathVariable Long id, @RequestBody io.zipcoder.persistenceapp.models.Employee employee){
-            // WILL NOT MODIFY THE ID BTW
-            return service.update(id, employee);
-        }
-
-        @DeleteMapping("/employees/{id}")
-        public Boolean delete(@PathVariable Long id){
-            return service.delete(id);
-        }
-    }
 }
